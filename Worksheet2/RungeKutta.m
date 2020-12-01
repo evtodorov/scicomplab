d7a063cba3c569a19c2372c0ca7c20a262e69d9f
etodorov_tum <evgeni.todorov@tum.de>
2020-12-01 16:51:54 +0000

    Clean-up RungeKutta

--- Worksheet2/RungeKutta.m
+++ Worksheet2/RungeKutta.m
@@ -7,12 +7,11 @@ ts = 0:dt:tend;
 yhat = zeros(1,length(ts));
 yhat(1)=y0;
 for i=2:length(ts)
-    tn = ts(i-1);
     yn = yhat(i-1);
-    Y1 = fy(tn,yn);
-    Y2 = fy(tn+dt/2.,yn+Y1*dt/2.);
-    Y3 = fy(tn+dt/2.,yn+Y2*dt/2.);
-    Y4 = fy(ts(i),yn+Y3*dt/2.);
+    Y1 = fy(yn);
+    Y2 = fy(yn+Y1*dt/2.);
+    Y3 = fy(yn+Y2*dt/2.);
+    Y4 = fy(yn+Y3*dt/2.);
     yhat(i) = yhat(i-1) + dt*(Y1+2*Y2+2*Y3+Y4)/6.;
 end
 
