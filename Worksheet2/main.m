ffa7d473be78fb514614b38984b2488bb960d24d
etodorov_tum <evgeni.todorov@tum.de>
2020-12-01 17:52:15 +0000

    implement pints iii and iv in main

--- Worksheet2/main.m
+++ Worksheet2/main.m
@@ -1,12 +1,14 @@
 %setup
-solvers = {@heunmethod};
-time_steps = [1, 0.5, 0.25, 1./8];
+solvers = {@ExplicitEuler, @heunmethod, @RungeKutta};
+time_steps = [1./8, 0.25, 0.5, 1];
+time_steps = sort(time_steps);
 fp = @(p) (1.-p/10.)*p;
 tend = 5;
 p0 = 1;
 
 
 for sol=1:length(solvers)
+    % setup table
     tab = table('Size',[4,4],...
                 'VariableTypes',["double" "double" "double" "double"],...
                 'VariableNames',["dt" "error" "error red." "error app."]);
@@ -14,12 +16,22 @@ for sol=1:length(solvers)
     plot(0:0.01:tend, pexact(0.01, tend),'k');
     hold on;
     i = 1;
+    %TODO: calculate eold
+    yhat = solvers{sol}(fp,p0,min(time_steps)/2.,tend);
+    e = approximationError(yhat, pexact(min(time_steps)/2,tend), min(time_steps)/2, tend);
     for dt=time_steps
         yhat = solvers{sol}(fp,p0,dt,tend);
+        if dt==min(time_steps)
+            pkbest = yhat;
+        end
         plot(0:dt:tend,yhat,'--');
+        eold = e;
         e = approximationError(yhat, pexact(dt,tend), dt, tend);
+        ehat = approximationError(yhat, pkbest(1:2^(i-1):end), dt, tend);
         tab(i,1) = {dt};
         tab(i,2) = {e};
+        tab(i,3) = {eold/e};
+        tab(i,4) = {ehat};
         i = i+1;
     end
     legend(['analytical', string(time_steps)],'location','southeast');
