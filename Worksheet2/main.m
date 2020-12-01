5410219c8765f5875031634dce97e1847ebcc539
etodorov_tum <evgeni.todorov@tum.de>
2020-12-01 16:00:50 +0000

    Fix list of function handles in main

--- Worksheet2/main.m
+++ Worksheet2/main.m
@@ -1,12 +1,12 @@
 %setup
-solvers = [@RungeKutta];
+solvers = {@RungeKutta};
 time_steps = [1, 0.5, 0.25, 1./8];
 fp = @(t,p) (1.-p/10.)*p;
 tend = 5;
 p0 = 1;
 
 
-for sol=solvers
+for sol=1:length(solvers)
     tab = table('Size',[4,4],...
                 'VariableTypes',["double" "double" "double" "double"],...
                 'VariableNames',["dt" "error" "error red." "error app."]);
@@ -15,7 +15,7 @@ for sol=solvers
     hold on;
     i = 1;
     for dt=time_steps
-        yhat = sol(fp,p0,dt,tend);
+        yhat = solvers{sol}(fp,p0,dt,tend);
         plot(0:dt:tend,yhat,'--');
         e = approximationError(yhat, pexact(dt,tend), dt, tend);
         tab(i,1) = {dt};
@@ -24,4 +24,5 @@ for sol=solvers
     end
     legend(['analytical' string(time_steps)],'location','southeast');
     hold off;
+    tab
 end
