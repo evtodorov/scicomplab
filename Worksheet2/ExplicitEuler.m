597dacfee90ef734ac1be7b4c09f3e9f7559d5a5
Nigel <ge59sum@tum.de>
2020-12-01 20:12:04 +0300

    implemented Explicit Euler

--- Worksheet2/ExplicitEuler.m
+++ Worksheet2/ExplicitEuler.m
@@ -1,7 +1,13 @@
 function yhat = ExplicitEuler(fy,y0,dt,tend)
+
 %ExplicitEuler Implement Explicit Euler method
 %   Detailed explanation goes here
-
-
+ts = 0:dt:tend;
+yhat = zeros(1,length(ts));
+yhat(1) = y0;
+for i=2:length(ts)
+    yn = yhat(i-1);
+    yhat(i) = yn+ dt*fy(yn);
 end
 
+end
---
+++ Worksheet2/worksheet2euler.m
@@ -0,0 +1,53 @@
+clear;
+clc;
+
+tend = 5;
+dt = 0.125;
+
+%yhat = ExplicitEuler(fy,y0,dt,tend)
+%ExplicitEuler Implement Explicit Euler method
+%   Detailed explanation goes here
+ts = 0:dt:tend;
+yo = 1;
+yhat = zeros(1,length(ts));
+yhat(1) = yo;
+for i=2:length(ts)
+    ydot = (1 - yhat(i-1)/10)*yhat(i-1);
+    yhat(i) = yhat(i-1)+ dt*ydot;
+end
+plot(ts,yhat);
+hold on;
+
+
+
+
+% 
+% %%%heun solution
+% for i=2:length(ts)
+%     ydot = (1 - yhat(i-1)./10)*yhat(i-1);
+%     yhat(i) = yhat(i-1)+ dt*ydot;
+%     ynextdot = (1 - yhat(i)./10)*yhat(i);
+%     yhat(i) = yhat(i-1)+ dt*(ydot+ynextdot)/2;
+% end
+% plot(ts,yhat);
+% hold on;
+% 
+% 
+% % runge kutta solution
+% for i=2:length(ts)
+%     y1dot = (1 - yhat(i-1)./10)*yhat(i-1);
+%     yhat(i) = yhat(i-1)+ dt/2*y1dot;
+%     y2dot = (1 - yhat(i)./10)*yhat(i);
+%     yhat(i) = yhat(i-1)+ dt/2*y2dot;
+%     y3dot = (1 - yhat(i)./10)*yhat(i);
+%     yhat(i) = yhat(i-1)+ dt*y3dot;
+%     y4dot = (1 - yhat(i)./10)*yhat(i);
+%     yhat(i) = yhat(i-1)+ dt*(y1dot+2*y2dot+2*y3dot+y4dot)/6;
+% end
+% plot(ts,yhat);
+% hold on;
+% 
+% %%exact solution
+pExact = 10./(1+9.*exp(-ts));
+plot(ts,pExact);
+hold on;
