3145df0fbcd4983975057cd804a44c8e3790b303
Christina Nuss-Brill <ge64lav@my.tum.de>
2021-01-13 18:36:01 +0100

    quick changes to Heat equation2. Added new concepts to GauSeidelSolver

--- Worksheet4/GaussSeidelSolver.m
+++ Worksheet4/GaussSeidelSolver.m
@@ -1,19 +1,19 @@
 function GaussSeidelSolver(Nx,Ny,b)
-b=zeros(Nx*Ny);
+b=zeros(nx*ny);
 hx=1/(Nx+1);
 hy=1/(Ny+1);
 %Initial Guesses
 
-X(i*2Ny)=1
-X(i*3Ny)=1
-X(i*Ny+j)=1
+X(i)=1
+X(i-(ny+2))=1
+X(i+(Ny+2))=1
 X(i*Ny+j+1)=1
 X(i*Ny+j+2)1
 
 %Solve the system of equations for each variable
-for i=0:nx
-    for j=0:Ny+1
-    X(i*Ny)=b(i)*hx^2/( 
+for i=1:(nx+2)*(ny+2)
+    x(i)=0;
+    X(i-(Ny+2))=b(i)*hx^2-X(i+(Ny+2))+ 2*X(i)-(X(i-1)-2*X(i)+X(i+1))/hy^2
     X(i*2Ny)=b(i)(X(i*Ny)/hx^2+X(i*3Ny)
     X(i*3Ny)=b(i)*hx/(
     X(i*Ny+j)=b(i)*hx/(
--- Worksheet4/HeatEquation2.m
+++ Worksheet4/HeatEquation2.m
@@ -1,4 +1,4 @@
-function yhat = HeatEquation(Nx,Ny)
+function yhat = HeatEquation2(Nx,Ny)
 %initialize A
 nx=Nx+2;
 ny=Ny+2;
@@ -9,12 +9,12 @@ hy=1/(Ny+1);
 %Create A matrix
 
     
-   for i=Ny+2:(Ny+2):nx*ny-1
+   for i=ny+3:ny:(nx*ny)-2
    A(i,i-1)=1/(hy^2);
    A(i,i+1)=1/(hy^2);
    A(i,i)=-2/(hx^2)-2/(hy^2);
-   A(i,i-(Ny+1))=1/(hx^2);
-   A(i,i+(Ny+1))=1/(hx^2);
+   A(i,i-(ny))=1/(hx^2);
+   A(i,i+(ny))=1/(hx^2);
     end
 
 A
