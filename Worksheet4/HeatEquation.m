4def8130004959af2b2b58bb7097b3dc4a03ea00
etodorov_tum <evgeni.todorov@tum.de>
2021-01-15 17:04:35 +0000

    Clean-up and add matrix equations

--- Worksheet4/HeatEquation.m
+++ Worksheet4/HeatEquation.m
@@ -1,26 +1,19 @@
 function A = HeatEquation(Nx,Ny)
 hx=1/(Nx+1);
 hy=1/(Ny+1);
-main_diag = (-2/(hx^2)-2/(hy^2))*ones(Nx*Ny,1);
-y_diag_p = ones(Nx*Ny,1)/(hy*hy);
-y_diag_p(Ny+1:Ny:end)=0;
-y_diag_m = ones(Nx*Ny,1)/(hy*hy);
-y_diag_m(Ny:Ny:end)=0;
-x_diag = ones(Nx*Ny,1)/(hx*hx);
 
 %wihtout BC
+main_diag = (-2/(hx^2)-2/(hy^2))*ones(Nx*Ny,1);
+y_diag_p = ones(Nx*Ny,1)/(hy*hy);
+y_diag_m = ones(Nx*Ny,1)/(hy*hy);
+x_diag = ones(Nx*Ny,1)/(hx*hx);
+
+% add BC
+y_diag_m(Ny:Ny:end)=0;
+y_diag_p(Ny+1:Ny:end)=0;
+
+% Construct sparse matrix
 A = spdiags([x_diag, y_diag_m, main_diag, y_diag_p, x_diag],...
             [ -Ny  ,   -1  ,     0    ,   1   ,    Ny  ],...
             Nx*Ny, Nx*Ny);
-        
-%apply BC
-% for i=1:(Nx*Ny)
-%     if i < Nx*Ny && rem(i,Ny)==0 % @ top     
-%         A(i,i+1) = 0;
-%     end
-%     if i > 1 && rem(i,Ny)==1 % @ bottom
-%         A(i, i-1) = 0;
-%     end
-%     %left & right are taken care of by the size of the matrix
-% end
 end
